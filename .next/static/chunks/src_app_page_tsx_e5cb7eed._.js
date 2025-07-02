43be8dea432a3366801709371ff392c9ac499d7b
Farjaadrizvi110 <farjaadrizvi@gmail.com>
2025-07-02 19:41:44 +0500

    Add .next/static/chunks/src_app_page_tsx_e5cb7eed._.js 1751467304375

---
+++ .next/static/chunks/src_app_page_tsx_e5cb7eed._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_1a2d665a._.js",
+  "static/chunks/src_app_page_tsx_b025fed5._.js",
+  "static/chunks/node_modules_0f33f316._.css"
+],
+    source: "dynamic"
+});
